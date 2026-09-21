45e6e19055e84e3a5ba217d41c628ec0b2a29f02
nsdichococravings <nsdi.chococravings2k24@gmail.com>
2026-09-21 16:06:48 +0530

    Test

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // NSDI ChocoCravings — Service Worker
-const CACHE_NAME = 'chococravings-v17-multi-item-payments';
+const CACHE_NAME = 'chococravings-v18-collections-close';
 const OFFLINE_URL = '/offline.html';
 
 // Files to cache immediately on install
